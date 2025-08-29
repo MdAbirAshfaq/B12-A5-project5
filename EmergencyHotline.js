797bffd115f0f08dca49bd5d34475ac272942891
Abir Ashfaq <abir.ashfaq16@gmail.com>
2025-08-29 16:36:33 +0600

    Add Card Section & Interactive JS

--- EmergencyHotline.js
+++ EmergencyHotline.js
@@ -1,28 +1,52 @@
-const serviceName = document.getElementById("service-name").innerText
-const serviceNumber = document.getElementById("service-number").innerText
+const serviceName1 = document.getElementById("service-name1").innerText
+const serviceName2 = document.getElementById("service-name2").innerText
+const serviceName3 = document.getElementById("service-name3").innerText
+const serviceName4 = document.getElementById("service-name4").innerText
+const serviceName5 = document.getElementById("service-name5").innerText
+const serviceName6 = document.getElementById("service-name6").innerText
+const serviceName7 = document.getElementById("service-name7").innerText
+const serviceName8 = document.getElementById("service-name8").innerText
+const serviceName9 = document.getElementById("service-name9").innerText
+
+const serviceNumber1 = document.getElementById("service-number1").innerText
+const serviceNumber2 = document.getElementById("service-number2").innerText
+const serviceNumber3 = document.getElementById("service-number3").innerText
+const serviceNumber4 = document.getElementById("service-number4").innerText
+const serviceNumber5 = document.getElementById("service-number5").innerText
+const serviceNumber6 = document.getElementById("service-number6").innerText
+const serviceNumber7 = document.getElementById("service-number7").innerText
+const serviceNumber8 = document.getElementById("service-number8").innerText
+const serviceNumber9 = document.getElementById("service-number9").innerText
+
+
 let coinNumber = parseInt(document.getElementById('coin').textContent)
 const heartBtns = document.querySelectorAll(".heart-btn")
 let historyData = [];
+
+
 // function for heart count
-document.getElementById("heart")
-    .addEventListener('click', function(){
-        const heartNumber = document.getElementById("heart-count")
-        let count = 0;
-        count++ 
-        heartNumber.innerText = count;
-    })
+let count = 0;
+
+    function incrementCounter() {
+      count += 1;
+      document.getElementById("heart-count").textContent = count;
+    }
+
+
+
 
 // function for call number
-document.getElementById("call-number")
+// card-1
+document.getElementById("call-number1")
     .addEventListener('click', function(){
-        alert(serviceName+ "\n" +serviceNumber);
+        alert(serviceName1+ "\n" +serviceNumber1);
         if (coinNumber >= 20) {
         coinNumber -= 20;
         document.getElementById('coin').textContent = coinNumber;
 
       const data = {
-        name: serviceName,
-        number: serviceNumber,
+        name: serviceName1,
+        number: serviceNumber1,
         time: new Date().toLocaleTimeString([], { 
           hour: '2-digit', 
           minute: '2-digit', 
@@ -38,6 +62,231 @@ document.getElementById("call-number")
       return; 
     }
     })
+
+
+// card-2
+document.getElementById("call-number2")
+    .addEventListener('click', function(){
+        alert(serviceName2+ "\n" +serviceNumber2);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName2,
+        number: serviceNumber2,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-3
+document.getElementById("call-number3")
+    .addEventListener('click', function(){
+        alert(serviceName3+ "\n" +serviceNumber3);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName3,
+        number: serviceNumber3,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-4
+document.getElementById("call-number4")
+    .addEventListener('click', function(){
+        alert(serviceName4+ "\n" +serviceNumber4);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName4,
+        number: serviceNumber4,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-5
+document.getElementById("call-number5")
+    .addEventListener('click', function(){
+        alert(serviceName5+ "\n" +serviceNumber5);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName5,
+        number: serviceNumber5,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-6
+document.getElementById("call-number6")
+    .addEventListener('click', function(){
+        alert(serviceName6+ "\n" +serviceNumber6);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName6,
+        number: serviceNumber6,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-7
+document.getElementById("call-number7")
+    .addEventListener('click', function(){
+        alert(serviceName7+ "\n" +serviceNumber7);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName7,
+        number: serviceNumber7,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-8
+document.getElementById("call-number8")
+    .addEventListener('click', function(){
+        alert(serviceName8+ "\n" +serviceNumber8);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName8,
+        number: serviceNumber8,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
+
+// card-9
+document.getElementById("call-number9")
+    .addEventListener('click', function(){
+        alert(serviceName9+ "\n" +serviceNumber9);
+        if (coinNumber >= 20) {
+        coinNumber -= 20;
+        document.getElementById('coin').textContent = coinNumber;
+
+      const data = {
+        name: serviceName9,
+        number: serviceNumber9,
+        time: new Date().toLocaleTimeString([], { 
+          hour: '2-digit', 
+          minute: '2-digit', 
+          second: '2-digit' 
+        })
+      };
+
+      historyData.push(data);
+      addHistory();
+
+    } else {
+      alert("Not enough coins!");
+      return; 
+    }
+    })
+
 
     // crate div 
 function addHistory() {
@@ -60,22 +309,110 @@ function addHistory() {
 }
 
 
+
+
 //function for copy number
-document.getElementById("copy-number")
+// card-1
+document.getElementById("copy-number1")
     .addEventListener('click', function(){
-        const number = serviceNumber; 
+        const number = serviceNumber1; 
         navigator.clipboard.writeText(number)
         .then(() => {
-          alert(" The number is copied:" + "\n" + serviceNumber);
+          alert(" The number is copied:" + "\n" + serviceNumber1);
         })
-        const copyCount = document.getElementById("copy-count")
-        let count = 0;
-        count++ 
-        copyCount.innerText = count;
     }
 )
 
 
+// card-2
+document.getElementById("copy-number2")
+    .addEventListener('click', function(){
+        const number = serviceNumber2; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber2);
+        })
+    }
+)
+// card-3
+document.getElementById("copy-number3")
+    .addEventListener('click', function(){
+        const number = serviceNumber3; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber3);
+        })
+    }
+)
+// card-4
+document.getElementById("copy-number4")
+    .addEventListener('click', function(){
+        const number = serviceNumber4; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber4);
+        })
+    }
+)
+// card-5
+document.getElementById("copy-number5")
+    .addEventListener('click', function(){
+        const number = serviceNumber5; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber5);
+        })
+    }
+)
+// card-6
+document.getElementById("copy-number6")
+    .addEventListener('click', function(){
+        const number = serviceNumber6; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber6);
+        })
+    }
+)
+// card-7
+document.getElementById("copy-number7")
+    .addEventListener('click', function(){
+        const number = serviceNumber7; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber7);
+        })
+    }
+)
+// card-8
+document.getElementById("copy-number8")
+    .addEventListener('click', function(){
+        const number = serviceNumber8; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber8);
+        })
+    }
+)
+// card-9
+document.getElementById("copy-number9")
+    .addEventListener('click', function(){
+        const number = serviceNumber9; 
+        navigator.clipboard.writeText(number)
+        .then(() => {
+          alert(" The number is copied:" + "\n" + serviceNumber9);
+        })
+    }
+)
+
+// ............................................
+let countCopy = 0;
+
+    function incrementCounterCopy() {
+      countCopy += 1;
+      document.getElementById("copy-count").textContent = countCopy;
+    }
+// .......................................................
 
 // delete all history
 document.getElementById("clear-history-btn")
